025f81c97886875d2265b5d7388196da071b5f16
Xuan <tanqazx@gmail.com>
2020-07-23 22:30:49 -0400

    Add residual resampling.

--- src/resample.jl
+++ src/resample.jl
@@ -1,16 +1,25 @@
 ## Resampling methods for SMC algorithms ##
-export pf_resample!, pf_multinomial_resample!, pf_stratified_resample!
+export pf_resample!
+export pf_residual_resample!, pf_multinomial_resample!, pf_stratified_resample!
 
 """
-    pf_resample!(state::ParticleFilterState, method=:multinomial)
+    pf_resample!(state::ParticleFilterState, method=:multinomial; kwargs...)
 
 Resamples particles in the filter, stochastically pruning low-weight particles.
-The resampling method can optionally be specified (default: multinomial).
+The resampling method can optionally be specified: `:multinomial` (default),
+`:residual`, or `:stratified`. See [1] for a survey of resampling methods
+ and their variance properties.
+
+[1] R. Douc and O. Cappé, "Comparison of resampling schemes
+for particle filtering," in ISPA 2005. Proceedings of the 4th International
+Symposium on Image and Signal Processing and Analysis, 2005., 2005, pp. 64–69.
 """
 function pf_resample!(state::ParticleFilterState,
                       method::Symbol=:multinomial; kwargs...)
     if method == :multinomial
-        return pf_multinomial_resample!(state)
+        return pf_multinomial_resample!(state; kwargs...)
+    elseif method == :residual
+        return pf_residual_resample!(state; kwargs...)
     elseif method == :stratified
         return pf_stratified_resample!(state; kwargs...)
     else
@@ -31,11 +40,48 @@ function pf_multinomial_resample!(state::ParticleFilterState)
     weights = exp.(log_weights)
     state.log_ml_est += log_total_weight - log(n_particles)
     # Resample new traces according to current normalized weights
-    rand!(Categorical(weights / sum(weights)), state.parents)
+    rand!(Categorical(weights), state.parents)
     for i=1:n_particles
-      state.new_traces[i] = state.traces[state.parents[i]]
-      state.log_weights[i] = 0.
+        state.new_traces[i] = state.traces[state.parents[i]]
+        state.log_weights[i] = 0.
     end
+    # Swap references
+    tmp = state.traces
+    state.traces = state.new_traces
+    state.new_traces = tmp
+    return state
+end
+
+"""
+    pf_residual_resample!(state::ParticleFilterState)
+
+Performs residual resampling of the particles in the filter, which reduces
+variance relative to multinomial sampling. For each particle with
+normalized weight ``w_i``, ``⌊n w_i⌋`` copies are resampled, where ``n`` is the
+total number of particles. The remainder are sampled with probability
+proportional to each particle's original weight.
+"""
+function pf_residual_resample!(state::ParticleFilterState)
+    n_particles = length(state.traces)
+    log_total_weight, log_weights = Gen.normalize_weights(state.log_weights)
+    weights = exp.(log_weights)
+    state.log_ml_est += log_total_weight - log(n_particles)
+    # Deterministically copy previous particles according to their weights
+    n_resampled = 0
+    for (i, w) in enumerate(weights)
+        n_copies = floor(Int, n_particles * w)
+        if n_copies == 0 continue end
+        state.parents[n_resampled+1:n_resampled+n_copies] .= i
+        state.new_traces[n_resampled+1:n_resampled+n_copies] .= state.traces[i]
+        n_resampled += n_copies
+    end
+    # Sample remainder at random
+    if n_resampled < n_particles
+        rand!(Categorical(weights), state.parents[n_resampled+1:end])
+        state.new_traces[n_resampled+1:end] =
+            state.traces[state.parents[n_resampled+1:end]]
+    end
+    state.log_weights .= 0.0
     # Swap references
     tmp = state.traces
     state.traces = state.new_traces
@@ -73,6 +119,7 @@ function pf_stratified_resample!(state::ParticleFilterState;
             accum_weight += weights[i_old+1]
             i_old += 1
         end
+        state.parents[i_new] = i_old
         state.new_traces[i_new] = state.traces[i_old]
         state.log_weights[i_new] = 0.0
     end
